d3fd20ef45a10eb8eb5c527bafddbb7e84e5e43c
Le Quang Thang <lethang7794@gmail.com>
2024-05-17 16:44:27 +0700

    preview help for complete command/option

--- sources/-command-.zsh
+++ sources/-command-.zsh
@@ -1,7 +1,17 @@
 # :fzf-tab:complete:(-command-:|command:option-(v|V)-rest)
 case $group in
 'external command')
-  less =$word
+  if bash -c "tldr $word" 2>/dev/null; then
+    eval "tldr $word"
+  elif bash -c "$word --help" 2>/dev/null; then
+    eval "$word --help" | bat -pl help
+  elif bash -c "$word -h" 2>/dev/null; then
+    eval "$word -h" | bat -pl help
+  elif bash -c "$word help" 2>/dev/null; then
+    eval "$word help" | bat -pl help
+  else
+    run-help $word | bat -lman
+  fi
   ;;
 'executable file')
   less ${realpath#--*=}
@@ -11,5 +21,8 @@ case $group in
   ;;
 parameter)
   echo ${(P)word}
+  ;;
+'shell function')
+  # TODO: Is there any way to print out the shell function?
   ;;
 esac
