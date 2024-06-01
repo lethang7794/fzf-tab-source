f40c2755fdb90863704366b6fdb8002908618839
Le Quang Thang <lethang7794@gmail.com>
2024-06-01 16:50:35 +0700

    git commands: show tldr if available

--- sources/git-help.zsh
+++ sources/git-help.zsh
@@ -1,2 +1,6 @@
 # :fzf-tab:complete:((\\|*/|)git|git-help):argument-1
-git help $word | bat -lhelp
+if bash -c "tldr git $word" >>/dev/null 2>&1; then
+  eval "tldr --color=always git $word"
+else
+  git help $word | bat -lhelp
+fi
