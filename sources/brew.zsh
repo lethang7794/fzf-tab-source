5065d8127b10cbe88386a85bd000860a3278f40a
Le Quang Thang <lethang7794@gmail.com>
2024-06-09 23:15:27 +0700

    brew: show tldr, fallback to help

--- sources/brew.zsh
+++ sources/brew.zsh
@@ -1,2 +1,8 @@
 # :fzf-tab:complete:(\\|*/|)brew:argument-1
-brew $word --help
+
+if bash -c "tldr $word" 1>/dev/null 2>&1; then
+  cat <(echo "\$ tldr $word") <(eval "tldr --color=always $word") <(echo)
+  hr
+fi
+
+cat <(echo "\$ brew $word --help") <(eval "brew $word --help")
