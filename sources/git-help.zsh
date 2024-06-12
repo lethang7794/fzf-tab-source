3ef692c1bd9cc9b82e1657a3645e2f43a8afae76
Le Quang Thang <lethang7794@gmail.com>
2024-06-12 18:19:57 +0700

    git-help: show both tldr and git-help

--- sources/git-help.zsh
+++ sources/git-help.zsh
@@ -1,6 +1,12 @@
 # :fzf-tab:complete:((\\|*/|)git|git-help):argument-1
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 if bash -c "tldr git $word" >>/dev/null 2>&1; then
-  eval "tldr --color=always git $word"
-else
-  git help $word | bat -lhelp
+  echo "$ tldr ${bold}git $word"
+  tldr --color=always git $word | tail -n +3
 fi
+
+echo "$ ${bold}git help $word"
+git help $word | bat -lhelp
