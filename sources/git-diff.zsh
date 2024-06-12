6bc08f093bfab680441590adf3c9f8440a877591
Le Quang Thang <lethang7794@gmail.com>
2024-06-12 18:20:19 +0700

    git-diff: use diff-so-fancy

--- sources/git-diff.zsh
+++ sources/git-diff.zsh
@@ -1,13 +1,18 @@
 # :fzf-tab:complete:git-(diff|cherry-pick):argument-rest
-case $group in
-'tree file')
-  less ${realpath#--*=}
-  ;;
-*)
-  if [ "$(command -v delta)" ]; then
-    git diff $realpath | delta --diff-so-fancy
-  else
-    git diff --color $realpath
-  fi
-  ;;
-esac
+# case $group in
+# 'tree file')
+#   less ${realpath#--*=}
+#   ;;
+# *)
+#   :
+#   ;;
+# esac
+
+if [ "$(command -v diff-so-fancy)" ]; then
+  git diff $realpath | diff-so-fancy
+else
+  git diff --color $realpath
+fi
+
+## delta wrap features is hard to read
+# git diff $realpath | delta --side-by-side
