3e36def00106332652d6fb0a321c4bda22adeede
Le Quang Thang <lethang7794@gmail.com>
2024-06-18 16:29:33 +0700

    feat: docker-image (command, repo, image), docker-image-inspect, docker-container-inspect, docker-container-logs

---
+++ sources/docker-argument-rest.zsh
@@ -0,0 +1,25 @@
+# :fzf-tab:complete:docker-:argument-rest
+echo ':fzf-tab:complete:docker-:argument-rest'
+
+if [[ $group == "docker image command" ]]; then
+  echo \$ docker image $word --help
+  docker image $word --help
+elif [[ $group == "repositories" ]]; then
+  echo \$ docker images $word
+  if [ "$(command -v docker-color-output)" ]; then
+    docker images $word | docker-color-output
+  else
+    docker images $word
+  fi
+elif [[ $group == "images" ]]; then
+  echo \$ docker image inspect $word
+  docker image inspect $word | bat -pl json
+elif [[ $words == "docker container inspect " ]]; then
+  echo \$ docker container inspect $word
+  docker container inspect $word | bat -pl json
+elif [[ $words == "docker container logs " ]]; then
+  echo \$ docker container logs $word
+  docker container logs $word | bat -pl log
+else
+  fzf-tab-source-debug
+fi
