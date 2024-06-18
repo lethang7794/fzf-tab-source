f1142d901dd68c8ebad5b3198bd38e6f6ed08d5e
Le Quang Thang <lethang7794@gmail.com>
2024-06-18 16:22:08 +0700

    feat(docker-container): initial version

--- sources/docker-container.zsh
+++ sources/docker-container.zsh
@@ -1,2 +1,20 @@
-# :fzf-tab:complete:docker-container:argument-1
-docker container $word --help
+# :fzf-tab:complete:docker-container:*
+echo ':fzf-tab:complete:docker-container:*'
+
+if [[ $group == "docker container command" ]]; then
+
+  if bash -c "tldr docker container $word" >>/dev/null 2>&1; then
+    echo "$ tldr docker container $word"
+    tldr --color=always docker container $word | tail -n +3
+  fi
+
+  echo \$ docker container $word --help
+  docker container $word --help
+
+elif [[ $group == "container" ]]; then
+  fzf-tab-source-debug
+elif [[ $group == "old name" ]]; then
+  fzf-tab-source-debug
+else
+  fzf-tab-source-debug
+fi
