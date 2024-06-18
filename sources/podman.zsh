38f220a24c73334662af1b42f34c41454a26ea4b
Le Quang Thang <lethang7794@gmail.com>
2024-06-19 03:05:37 +0700

    feat(podman): container

--- sources/podman.zsh
+++ sources/podman.zsh
@@ -1,5 +1,10 @@
 # :fzf-tab:complete:(\\|*/|)podman:*
 
+# Skip the options
+if [[ "$desc" =~ ^"-" ]]; then
+  return
+fi
+
 if [[ $words == "podman " ]]; then
   if bash -c "tldr podman $word" >/dev/null 2>&1; then
     echo "$ tldr podman $word"
@@ -27,6 +32,24 @@ elif [[ $words == "podman container " ]]; then
 
   echo \$ man podman-container-$word
   man podman-container-$word 2>/dev/null | bat -lman
+
+elif [[ $words =~ "podman container" ]]; then
+  if [[ $words =~ "attach|checkpoint|cleanup|clone|commit|cp|create|exec|export|init|inspect|kill|list|mount|pause|prune|ps|rename|restart|restore|rm|runlabel|start|stats|stop|unmount|unpause|update|wait" ]]; then
+    echo "$ podman container inspect $word"
+    podman container inspect $word | jq --color-output
+  elif [[ $words =~ "diff|exists|logs|port" ]]; then
+    echo "$ $words$word"
+    $words$word | jq --color-output
+  elif [[ $words =~ "run" ]]; then
+    echo "$ podman image inspect $word"
+    podman image inspect $word | jq --color-output
+  elif [[ $words =~ "top" ]]; then
+    echo "$ $words$word"
+    $words$word
+  else
+    debug
+  fi
+
 else
   debug
 fi
