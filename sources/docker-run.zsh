aadbfcc04bdac3d267af5d9fafe949efdb354ea9
Le Quang Thang <lethang7794@gmail.com>
2024-06-18 16:15:49 +0700

    feat(docker-run): support inspect individual image

--- sources/docker-run.zsh
+++ sources/docker-run.zsh
@@ -1,2 +1,19 @@
 # :fzf-tab:complete:docker-(run|images):argument-1
-docker images $word
+echo ':fzf-tab:complete:docker-(run|images):argument-1'
+
+if [[ $group == "repositories" ]]; then
+  echo \$ docker images $word
+  if [ "$(command -v docker-color-output)" ]; then
+    docker images $word | docker-color-output
+  else
+    docker images $word
+  fi
+elif [[ $group == "repositories with tags" ]]; then
+  echo \$ docker image inspect $word
+  docker image inspect $word | bat -pl json
+elif [[ $group == "images" ]]; then
+  echo \$ docker image inspect $word
+  docker image inspect $word | bat -pl json
+else
+  fzf-tab-source-debug
+fi
