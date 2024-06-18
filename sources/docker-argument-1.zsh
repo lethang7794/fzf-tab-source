edf283525d2ca84287929e25a4d593d1e43c525d
Le Quang Thang <lethang7794@gmail.com>
2024-06-18 16:26:28 +0700

    feat(docker-image): which one?

---
+++ sources/docker-argument-1.zsh
@@ -0,0 +1,19 @@
+# :fzf-tab:complete:docker-:argument-1
+echo ':fzf-tab:complete:docker-:argument-1'
+
+if [[ $group == "docker image command" ]]; then
+  echo \$ docker image $word --help
+  docker image $word --help
+elif [[ $group == "repositories" ]]; then
+  echo \$ docker images $word
+  if [ "$(command -v docker-color-output)" ]; then
+    docker images $word | docker-color-output
+  else
+    docker images $word
+  fi
+elif [[ $group == "images" ]]; then
+  echo \$ docker image inspect $word
+  docker image inspect $word | bat -pl json
+else
+  fzf-tab-source-debug
+fi
