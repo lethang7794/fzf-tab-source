61212f87b711ea39ca3ce7debaec2542e0013f63
Le Quang Thang <lethang7794@gmail.com>
2024-06-18 16:23:32 +0700

    feat(docker-diff): initial version

---
+++ sources/docker-diff.zsh
@@ -0,0 +1,14 @@
+# :fzf-tab:complete:docker-diff:*
+echo ':fzf-tab:complete:docker-diff:*'
+
+if [[ "${group}" == "containers" ]]; then
+  echo \$ docker diff $word
+  diff=$(docker diff $word)
+  if [[ -z "${diff}" ]]; then
+    echo "No changes to files or directories on the container's filesystem"
+  else
+    echo $diff
+  fi
+else
+  fzf-tab-source-debug
+fi
