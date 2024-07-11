0cf1a21a3570eb59352d89be1cfaad383d94f410
Le Quang Thang <lethang7794@gmail.com>
2024-07-11 23:47:32 +0700

    perf(packer): remove unnessesary if

--- sources/packer.zsh
+++ sources/packer.zsh
@@ -12,10 +12,8 @@ fi
 
 # Sub-command, e.g. packer build
 if [ $level = 1 ]; then
-  if bash -c "packer $word --help" >/dev/null 2>&1; then
-    echo "$ packer $word --help" | bat -pl bash
-    packer $word --help | bat -l help
-  fi
+  echo "$ packer $word --help" | bat -pl bash
+  packer $word --help | bat -l help
 fi
 
 if [ $level = 2 ]; then
@@ -23,10 +21,8 @@ if [ $level = 2 ]; then
 
   # Sub-command with nested sub-command, e.g. packer plugins install
   if [[ $words =~ " (plugins) " ]]; then
-    if bash -c "packer $cmd $word --help" >/dev/null 2>&1; then
-      echo "$ packer $cmd $word --help" | bat -pl bash
-      packer $cmd $word --help | bat -l help
-    fi
+    echo "$ packer $cmd $word --help" | bat -pl bash
+    packer $cmd $word --help | bat -l help
   fi
 
 fi
