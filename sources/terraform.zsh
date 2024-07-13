8e383d37a31d2b35c87b19bbc009068d001dbb5e
Le Quang Thang <lethang7794@gmail.com>
2024-07-13 13:49:05 +0700

    feat: terraform

---
+++ sources/terraform.zsh
@@ -0,0 +1,28 @@
+# :fzf-tab:complete:(\\|*/|)terraform:*
+
+local level=$(echo "$words" | tr -cd ' ' | wc -c)
+
+# Trim trailing whitespace
+local word=$(echo $word | xargs)
+
+# Skip preview for the options
+if [[ $word == "-"* ]]; then
+  return
+fi
+
+# Sub-command, e.g. terraform apply
+if [ $level = 1 ]; then
+  echo "$ terraform $word --help" | bat -pl bash
+  terraform $word --help | bat -l help
+fi
+
+# Sub-command with nested sub-command, e.g. terraform metadata functions
+if [ $level = 2 ]; then
+  local cmd=$(echo $words | awk '{print $2}')
+
+  if [[ $words =~ " (metadata|providers|state|workspace) " ]]; then
+    echo "$ terraform $cmd $word --help" | bat -pl bash
+    terraform $cmd $word --help | bat -l help
+  fi
+
+fi
