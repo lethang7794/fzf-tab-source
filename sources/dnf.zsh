92af3d75f0608bc16a627f5fb01f5ca54363d87d
Le Quang Thang <lethang7794@gmail.com>
2024-07-21 23:27:16 +0700

    refactor(dnf): use command level to determine sub-command

--- sources/dnf.zsh
+++ sources/dnf.zsh
@@ -1,3 +1,35 @@
-# :fzf-tab:complete:(\\|*/|)dnf:argument-rest
+# :fzf-tab:complete:(\\|*/|)dnf:*
 
-dnf $word --help
+# debug
+
+local level=$(echo "$words" | tr -cd ' ' | wc -c)
+
+# Trim trailing whitespace
+local word=$(echo $word | xargs)
+
+# Skip preview for the options
+if [[ $word == "-"* ]]; then
+  return
+fi
+
+# Sub-command, e.g. dnf install
+if [ $level = 1 ]; then
+  if bash -c "tldr dnf $word" >/dev/null 2>&1; then
+    echo "$ tldr dnf $word"
+    tldr --color=always dnf $word | tail -n +3
+  fi
+
+  echo "$ dnf $word --help" | bat -pl bash
+  dnf $word --help | bat -l help
+fi
+
+# Sub-command with nested sub-command, e.g. dnf metadata functions
+if [ $level = 2 ]; then
+  local cmd=$(echo $words | awk '{print $2}')
+
+  if [[ $words =~ " (info|install|reinstall|remove|upgrade|downgrade|list|swap) " ]]; then
+    echo "$ dnf5 -C $cmd $word" | bat -pl bash
+    dnf5 -C $cmd $word | bat -pl yaml
+  fi
+
+fi
