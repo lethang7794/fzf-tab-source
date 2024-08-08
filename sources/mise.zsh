46286220d82cb99100b8ac063163fc3690739191
Le Quang Thang <lethang7794@gmail.com>
2024-08-08 16:08:29 +0700

    feat(mise): show help for --help and help command

--- sources/mise.zsh
+++ sources/mise.zsh
@@ -1,27 +1,32 @@
 # :fzf-tab:complete:mise:*
 
 local level=$(echo "$words" | tr -cd ' ' | wc -c)
+local prefix="${words% *}"
+local word="${word% *}"
 
-# Trim trailing whitespace
-local word=$(echo $word | xargs)
-
-# Skip the option
+# Show command's help for --help or -h option
 if [[ $word == "-"* ]]; then
+  if [[ $word == "--help" ]] || [[ $word == "-h" ]]; then
+    if bash -c "$prefix $word" >/dev/null 2>&1; then
+      echo "$ $prefix $word"
+      eval $prefix $word | bat -l help
+    fi
+  fi
   return
 fi
 
 # Level 1 command, e.g. mise install
 if [ $level = 1 ]; then
   if [[ $word =~ "help" ]]; then
-    echo "$ mise $cmd $word" | bat -pl bash
-    mise $cmd $word | bat -l help
+    echo "$ mise $word" | bat -pl bash
+    mise $word | bat -l help
   else
     echo "$ mise $word --help"
     mise $word --help
   fi
 fi
 
-# Level 2, e.g. mise backends
+# Level 2, e.g. mise backends ls
 if [ $level = 2 ]; then
   local cmd=$(echo $words | awk '{print $2}')
 
@@ -33,5 +38,10 @@ if [ $level = 2 ]; then
       echo "$ mise $cmd $word --help" | bat -pl bash
       mise $cmd $word --help | bat -l help
     fi
+  fi
+
+  if [[ $words =~ " (help) " ]]; then
+    echo "$ mise help $word" | bat -pl bash
+    mise help $word | bat -l help
   fi
 fi
