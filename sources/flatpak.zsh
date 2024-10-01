a0b82d893814a6891b3fd8229e5b601b89f1e87a
Le Quang Thang <lethang7794@gmail.com>
2024-10-01 15:52:22 +0700

    feat: flatpak

--- sources/flatpak.zsh
+++ sources/flatpak.zsh
@@ -1,16 +1,53 @@
-# :fzf-tab:complete:(\\|*/|)flatpak:
-# echo $word $group $realpath
-# case $group in
-# argument)
-#   flatpak $word --help
-#   ;;
-# esac
+# :fzf-tab:complete:(\\|*/|)flatpak:*
 
 # Trim trailing whitespace
 local word=$(echo $word | xargs)
 
-if [[ "${word}" == *"."* ]]; then
-  flatpak info $word | bat -pl yaml
-else
-  flatpak $word --help
+local level=$(echo "$words" | tr -cd ' ' | wc -c)
+
+# Trim trailing whitespace
+local word=$(echo $word | xargs)
+
+# Skip preview for the options
+if [[ $level == 1 ]] && [[ $word == "-"* ]]; then
+  return
+fi
+
+# e.g. flatpak CMD
+if [ $level = 1 ]; then
+  if bash -c "tldr flatpak $word" >/dev/null 2>&1; then
+    echo "$ tldr flatpak $word"
+    tldr --color=always flatpak $word | tail -n +3
+  fi
+
+  echo "$ flatpak $word --help" | bat -pl bash
+  flatpak $word --help | bat -l help
+fi
+
+# e.g. dnf info PACKAGE
+if [ $level = 2 ]; then
+  local cmd=$(echo $words | awk '{print $2}')
+
+  if [[ $word == "-"* ]]; then
+    flatpak $cmd --help | bat -l help
+    return
+  fi
+
+  if [[ $words =~ " (info) " ]]; then
+    echo "\$ flatpak info $word"
+    flatpak info $word | bat -pl yaml
+  fi
+fi
+
+# e.g. dnf install fedora PACKAGE
+if [[ $level = 3 ]]; then
+  if [[ $words =~ " (install|remote-info) " ]]; then
+    local remote=$(echo $words | awk '{print $3}')
+
+    echo "\$ flatpak remote-ls $remote | grep "$word"" | bat -pl bash
+    flatpak remote-ls $remote | grep "$word"
+    echo
+    echo "\$ flatpak remote-info $remote $word" | bat -pl bash
+    flatpak remote-info $remote $word | bat -pl yaml
+  fi
 fi
